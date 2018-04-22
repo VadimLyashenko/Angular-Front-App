ed2fbb8df27e1b19e54ea2c23f671d9819be960a
Worst <terateratera23@gmail.com>
2018-04-22 19:54:28 +1000

    update menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 class Category {
-	id: number;
-	name: string;
+	title: string;
+	positions: Position[];
 }
 
 class Position {
-	id: number;
-	name: string;
+	title: string;
 	cost: string;
 	desc: string;
 	weight: string;
 	img: string;
-	id_category: number;
+	count: number; 
 }
 
+class UserCart {
+	img: string;
+	title: string;
+	cost: string;
+	count: number;
+}
 
 @Component({
   selector: 'app-menu',
@@ -22,43 +27,79 @@ class Position {
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+	
+	sel_category: any;
+	cart: UserCart[] = [];
+	// = [{
+	 // img: "",
+	 // title: "",
+	 // cost: "",
+	 // count: 0
+	// }];
 
 	categories: Category[] = [
-  { id: 1, name: 'Первые блюда' },
-  { id: 2, name: 'Вторые блюда' },
-  { id: 3, name: 'III блюда' }
+  { title: 'Первые блюда', 
+  	positions:  [ 
+  		{ 
+  			title	 : "Томатный суп",
+  			cost 	 : "300р", 
+  			desc 	 : "ингридиент-1, ингиеыыснт-2, ингридиент-3",
+				weight : "300г",
+				img		 : "assets/img/tomato-soup.png",
+				count	 : 1
+			},
+			{ 
+  			title	 : "Череп суп",
+  			cost 	 : "500р", 
+  			desc 	 : "мясо0, ууууунгридиент-3",
+				weight : "567г",
+				img		 : "assets/img/tomato-soup.png",
+				count	 : 1
+			}
+		]
+	},
+	{ title: 'Вторые блюда', 
+  	positions:  [ 
+  		{ 
+  			title	 : "Каша",
+  			cost 	 : "545р", 
+  			desc 	 : "ингридиент-5, ингиеыыснт-6, ингридиент-7",
+				weight : "300г",
+				img		 : "assets/img/salat.jpg",
+				count	 : 1
+			},
+			{ 
+  			title	 : "ФЫВАП",
+  			cost 	 : "3238р", 
+  			desc 	 : "йцукенгоримсывамс, вмв",
+				weight : "4534г",
+				img		 : "assets/img/meet.jpg",
+				count	 : 1
+			}
+		]
+	}
   ];
-
-  positions: Position[] = [ { id: 1, name: "Томатный суп", cost: "300р", 
-  	desc 	 : "ингридиент-1, ингиеыыснт-2, ингридиент-3",
-		weight : "300г",
-		img		 : "assets/img/tomato-soup.png",
-		id_category: 1},
-		{ id: 2, name: "Каша", cost: "1500р", 
-  	desc 	 : "ингриди2222нт-1, ингиеыыснт-45, ингридиент-3",
-		weight : "2000г",
-		img		 : "assets/img/tomato-soup.png",
-		id_category: 1},
-		{ id: 2, name: "Каша", cost: "1500р", 
-  	desc 	 : "ингриди2222нт-1, ингиеыыснт-45, ингридиент-3",
-		weight : "2000г",
-		img		 : "assets/img/tomato-soup.png",
-		id_category: 1},
-		{ id: 2, name: "Каша", cost: "1500р", 
-  	desc 	 : "ингриди2222нт-1, ингиеыыснт-45, ингридиент-3",
-		weight : "2000г",
-		img		 : "assets/img/tomato-soup.png",
-		id_category: 1},
-		{ id: 2, name: "Каша", cost: "1500р", 
-  	desc 	 : "ингриди2222нт-1, ингиеыыснт-45, ингридиент-3",
-		weight : "2000г",
-		img		 : "assets/img/tomato-soup.png",
-		id_category: 1}
-  ]
   
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSelect(sel_category): void {
+ 		this.sel_category = sel_category;
+	}
+
+	onMinus(item): void {
+		if(item.count >1)
+ 		item.count--;
+	}
+
+	onPlus(item): void {
+ 		item.count++;
+	}
+
+  inCart(item): void {
+ 		this.cart.push({img: item.img, title: item.title, cost: item.cost, count: item.count });
+ 	}
 
 }
